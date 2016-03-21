d4f6d4773c3041b13dad2bc8a0df6220ef9abbdd
ushchent <am@opendata.by>
2016-03-21 21:01:15 +0300

    Обновлена галерея.

--- js/script.js
+++ js/script.js
@@ -93,6 +93,7 @@ button.onclick = function() {
 
 
 window.onload = function() {
+  var today = new Date();
 
   var request = new XMLHttpRequest();
   var gallery = document.getElementById("gallery");
@@ -100,14 +101,60 @@ window.onload = function() {
   request.onreadystatechange = function() {
       if (request.readyState === 4) {
           jsonData = JSON.parse(request.responseText);
+          jsonData.sort(function(a, b) { 
+            return new Date(a.data) - new Date(b.data);
+          });
+          var aktualno = jsonData.filter(function(b) { if (new Date(b.data) >= today) {return b; }; });
+
+          var nearest = [];
+          var firstItem = aktualno.shift();
+          nearest.push(firstItem);
           
-          for (var i = 0; i < 5; i++) {
+          var headerTime = document.getElementById("nearest");
+          var nearestTimeText = document.createTextNode(firstItem.data);
+          headerTime.appendChild(nearestTimeText);
+          
+          var plus = aktualno.filter(function(d) { return d.data == firstItem.data; });
+          console.log(plus.length);
+          
+          var final = nearest.concat(plus);
+          console.log(final.length);
+          
+          
+          for (var i = 0; i < final.length; i++) {
               var face = document.createElement("div");
               var image = document.createElement("img");
-              image.setAttribute("src", "img/" + jsonData[i].photo);
-              image.setAttribute("title", jsonData[i].fio);
+              image.setAttribute("src", "img/" + final[i].photo);
+              image.setAttribute("title", final[i].fio + " - " + final[i].data);
+              
+              var organHeader = document.createElement("h3");
+              var organText = document.createTextNode(final[i].organ);
+              organHeader.appendChild(organText);
+              
+              var positionPar = document.createElement("p");
+              var positionText = document.createTextNode(final[i].dolzhnost);
+              positionPar.appendChild(positionText);
+              
+              var fioPar = document.createElement("p");
+              var fioText = document.createTextNode(final[i].fio);
+              fioPar.appendChild(fioText);
+              
+              var timePar = document.createElement("p");
+              var timeText = document.createTextNode(final[i].vremia);
+              timePar.appendChild(timeText);
+
+              var telPar = document.createElement("p");
+              var telText = document.createTextNode(final[i].tel);
+              telPar.appendChild(telText);
+              
+              face.appendChild(organHeader);
               
               face.appendChild(image);
+              face.appendChild(fioPar);
+
+              face.appendChild(positionPar);
+              face.appendChild(timePar);
+              face.appendChild(telPar);
               gallery.appendChild(face);
           }
 
